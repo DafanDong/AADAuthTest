332ecad19ca7bf3630f05703cdd6a39104ec7029
Dwayne Dong <dadon@microsoft.com>
2015-09-26 00:01:58 +0800

    successfully get Access token from client

--- solution/Client/MainWindow.xaml.cs
+++ solution/Client/MainWindow.xaml.cs
@@ -13,6 +13,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// The following using statements were added for this sample.
+using System.Globalization;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Net.Http;
+using System.Net.Http.Headers;
+//using System.Web.Script.Serialization;
+using System.Runtime.InteropServices;
+using System.Configuration;
+
 namespace Client
 {
     /// <summary>
@@ -20,9 +29,60 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        //
+        // The Client ID is used by the application to uniquely identify itself to Azure AD.
+        // The Tenant is the name of the Azure AD tenant in which this application is registered.
+        // The AAD Instance is the instance of Azure, for example public Azure or Azure China.
+        // The Redirect URI is the URI where Azure AD will return OAuth responses.
+        // The Authority is the sign-in URL of the tenant.
+        //
+        private static string aad_login = ConfigurationManager.AppSettings["ida:AADLogin"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        Uri redirectUri = new Uri(ConfigurationManager.AppSettings["ida:RedirectUri"]);
+
+        private static string authority = String.Format(CultureInfo.InvariantCulture, aad_login, tenant);
+
+        //
+        // To authenticate to the To Do list service, the client needs to know the service's App ID URI.
+        // To contact the To Do list service we need it's URL as well.
+        //
+        private static string resource_id = ConfigurationManager.AppSettings["ida:ResourceId"];
+
+        private HttpClient httpClient = new HttpClient();
+        private AuthenticationContext authContext = null;
         public MainWindow()
         {
             InitializeComponent();
+            //
+            // As the application starts, try to get an access token without prompting the user.  If one exists, populate the To Do list.  If not, continue.
+            //
+            authContext = new AuthenticationContext(authority);
+            AuthenticationResult result = null;
+            try
+            {
+                result = authContext.AcquireToken(resource_id, clientId, redirectUri);
+                string text = "Access token: ";
+                token.Text = (text + result.AccessToken);
+            }
+            catch (AdalException ex)
+            {
+                if (ex.ErrorCode == "user_interaction_required")
+                {
+                    // There are no tokens in the cache.  Proceed without calling the To Do list service.
+                }
+                else
+                {
+                    // An unexpected error occurred.
+                    string message = ex.Message;
+                    if (ex.InnerException != null)
+                    {
+                        message += "Inner Exception : " + ex.InnerException.Message;
+                    }
+                    MessageBox.Show(message);
+                }
+                return;
+            }
         }
     }
 }
